4218c2987bfd50440b4c4bf5ad5ffe4fd4ae25dc
Jonda <jonda@example.com>
2026-07-22 20:18:39 +0200

    ejs stranka

--- src/server.js
+++ src/server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('Miluj svuj hydrant - zaklad bezi. Sem prijde vypis hydrantu.');
+    res.render('index', { title: 'Domovská stránka' });
 });
 
 app.listen(PORT, () => {
     console.log(`Server bezi na http://localhost:${PORT}`);
 });
+
+app.set('views', path.join(__dirname, 'views'));

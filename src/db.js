5e1e6b4c5ab8420eeed2f1f3896f5f06a9ff953c
Jonáš Hubený <jonas.hubeny@gmail.com>
2026-09-17 08:29:01 +0200

    fix db.js

--- src/db.js
+++ src/db.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
